54dc14647f954d91b813dac1d2beaeecd28d70f4
anilpandey071999 <anilpandey071999@gmail.com>
2024-02-06 23:37:50 +0530

    feat: add echo tcp server

--- cmd/keydbservers/main.go
+++ cmd/keydbservers/main.go
@@ -1,35 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/anilpandey071999/keydb/pkg/storage"
+	"net"
+	"os"
 )
 
 func main() {
-	kvStore := storage.NewKeyValueStore()
+	// kvStore := storage.NewKeyValueStore()
 
-	var key string
-	var values string
-
-	fmt.Println("you Key")
-	fmt.Scanln(&key)
-	
-	fmt.Println("you Value")
-	fmt.Scanln(&values)
-	
-	kvStore.Set(key, values)
-	
-	value, exists := kvStore.Get(key)
-	if exists {
-		fmt.Println("Retrived:", value)
+	fmt.Println("Lounching the Service...")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	
-	kvStore.Delete(key)
-	
-	_, exists = kvStore.Get(key)
-	if !exists {
-		fmt.Println("Key1 Deleted")
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Errorf("unimplemented %s", err.Error())
+		}
+		handleConnection(conn)
 	}
+}
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Connection Colsed", err.Error())
+			return
+		}
+		fmt.Println("Message Recived: ", message, string(message))
+		conn.Write([]byte(message))
+	}
 }
